091f5c8df0158feea9b640b35fc01a96640f8256
dawnaldrich <dawnstarr.aldrich@gmail.com>
2018-06-18 14:13:15 -0700

    06-18-2018__2:11pm working on profile

---
+++ src/actions/profile-actions.js
@@ -0,0 +1,43 @@
+import superagent from 'superagent';
+import * as routes from '../routes';
+
+const setProfile = profile => ({
+  type: 'CLIENT_PROFILE_SET',
+  payload: profile,
+});
+
+const createRequestProfile = profile => (store) => {
+  const { token } = store.getState();
+
+  return superagent.post(`${API_URL}${routes.PROFILE_ROUTE}`)
+    .set('Authorization', `Bearer ${token}`)
+    .set('Content-Type', 'application/json')
+    .send(profile)
+    .then((response) => {
+      return store.dispatch(setProfile(response.body));
+    });
+};
+
+const updateRequestProfile = profile => (store) => {
+  const { token } = store.getState();
+
+  return superagent.put(`${API_URL}${routes.PROFILE_ROUTE}/${profile._id}`)
+    .set('Authorization', `Bearer ${token}`)
+    .set('Content-Type', 'application/json')
+    .send(profile)
+    .then((response) => {
+      return store.dispatch(setProfile(response.body));
+    });
+};
+
+const fetchRequestProfile = () => (store) => {
+  const { token } = store.getState();
+
+  return superagent.get(`${API_URL}${routes.PROFILE_ROUTE}/me`)
+    .set('Authorization', `Bearer ${token}`)
+    .then((response) => {
+      return store.dispatch(setProfile(response.body));
+    });
+};
+
+export { setProfile, createRequestProfile, updateRequestProfile, fetchRequestProfile };
---
+++ src/components/profile/profile.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import * as profileActions from '../../actions/profile-actions';
+import * as routes from '../../routes';
+
+import autoBind from '../../utils/auto-bind';
+import ProfileForm from '../profile-form/profile-form';
+
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: false,
+    };
+    autoBind.call(this, Profile);
+  }
+
+  handleCreate(profile) {
+    this.props.profileCreate(profile)
+      .then(() => {
+        this.props.history.push(routes.DASHBOARD_ROUTE);
+      });
+  }
+
+  handleUpdate(profile) {
+    this.props.profileUpdate(profile);
+    this.setState({ editing: false });
+  }
+
+  render() {
+    const { profile } = this.props;
+
+    let JSXEditing = null;
+    let JSXDisplay = null;
+    let JSXProfile = null;
+
+    if (profile) {
+      JSXEditing =
+        <div>
+          <ProfileForm profile={profile} onComplete={this.handleUpdate}/>
+          <button onClick={() => this.setState({ editing: false })}>Cancel</button>
+        </div>;
+
+      JSXDisplay =
+        <div>
+          <button onClick={() => this.setState({ editing: true })}>Edit</button>
+        </div>;
+
+      JSXProfile =
+        <div>
+          <p>Username - {profile.username}</p>
+          <p>Email - {profile.email}</p>
+          <p>Phone Number - {profile.phoneNumber}</p>
+          {this.state.editing ? JSXEditing : JSXDisplay}
+        </div>;
+    }
+
+    return (
+      <div className="profile">
+        <h1>Profile</h1>
+        {
+          profile ? JSXProfile : <ProfileForm onComplete={this.handleCreate}/>
+        }
+      </div>
+    );
+  }
+}
+
+Profile.propTypes = {
+  profile: PropTypes.object,
+  profileCreate: PropTypes.func,
+  profileUpdate: PropTypes.func,
+  history: PropTypes.object,
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+});
+
+const mapDispatchToProps = dispatch => ({
+  profileCreate: profile => dispatch(profileActions.createRequestProfile(profile)),
+  profileUpdate: profile => dispatch(profileActions.updateRequestProfile(profile)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
