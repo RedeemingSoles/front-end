b2c68df7892e53706cc5e26aa80ea2b1f83cea74
dawnaldrich <dawnstarr.aldrich@gmail.com>
2018-06-19 10:13:26 -0700

    06-19-2018__10:13:AM donorshoes action and reducer

---
+++ src/reducers/donor-shoes.js
@@ -0,0 +1,29 @@
+export const validateDonorShoes = (payload) => {
+  if (!payload._id) {
+    throw new Error('Validation error, no id');
+  }
+
+  // TODO: Fix required properties when we refactor for the list in components
+
+  if (!payload.shoeType || !payload.age || !payload.gender || !payload.shoeSize) {
+    throw new Error('Missing required properties');
+  }
+};
+
+export default (state = [], { type, payload }) => {
+  switch (type) {
+    case 'SHOES_DONOR_FETCH':
+      return payload;
+    case 'SHOES_DONOR_CREATE':
+      validateDonorShoes(payload);
+      return [payload, ...state];
+    case 'SHOES_DONOR_UPDATE':
+      validateDonorShoes(payload);
+      return state.map(donorShoes => (donorShoes._id === payload._id ? payload : donorShoes));
+    case 'SHOES_DONOR_DELETE':
+      validateDonorShoes(payload);
+      return state.filter(donorShoes => donorShoes._id !== payload._id);
+    default:
+      return state;
+  }
+};
