d4a6d8405396c8a14f631206e3fa8a30d31ead0f
dawnaldrich <dawnstarr.aldrich@gmail.com>
2018-06-20 09:09:31 -0700

    admin-view sharing with Joshua

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-
+import Account from './account';
 import ShoesForm from '../shoes-form/shoes-form';
 import ShoesDonorForm from '../shoes-donor-form/shoes-donor-form';
 import * as shoesActions from '../../actions/shoes-actions';
+import * as routes from "../../routes";
 
 class Dashboard extends React.Component {
   render() {
@@ -12,11 +13,20 @@ class Dashboard extends React.Component {
       shoesCreate,
     } = this.props;
 
-    return (
-      <div className="dashboard">
+    const JSXAdminView =
+        <div>
         <h3>Request some shoes!</h3>
         <ShoesForm onComplete={shoesCreate} buttonText={'Submit'}/>
-        <ShoesDonorForm onComplete={shoesCreate} buttonText={'Submit'}/>
+          <ShoesDonorForm onComplete={shoesCreate} buttonText={'Submit'}/></div>
+
+    const JSXNotLoggedIn =
+      <div>
+        <h3>Request some shoes!</h3>
+        <ShoesForm onComplete={shoesCreate} buttonText={'Submit'}/></div>
+
+    return (
+      <div className="dashboard">
+        { this.account.isAdmin ? JSXAdminView : JSXNotLoggedIn }
       </div>
     );
   }
