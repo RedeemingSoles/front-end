ab30928afcfaa5608549979543e8864305cc5a56
Joshua Fredrickson <uafredrickson@gmail.com>
2018-06-22 02:08:22 -0700

    06-22-2018  02:08AM  fully removing devtools extension

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from './components/app/app';
 import reducers from './reducers/index';
@@ -12,7 +11,7 @@ import thunk from './lib/redux-thunk';
 
 import './style/main.scss';
 
-const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk, reporter, session)));
+const store = createStore(reducers, applyMiddleware(thunk, reporter, session));
 
 const container = document.createElement('div');
 document.body.appendChild(container);
